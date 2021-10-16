0ea3491cb5497478a12642cd9b58b0034e026206
Mthobisi-za <mthobisimtho333@gmail.com>
2021-10-16 23:40:59 +0200

    start script

--- tables.sql
+++ tables.sql
@@ -1,5 +1,5 @@
 create table users(
-    username text not null, password text not null, age int, gender text, contact text, profile text, id seriel primary key
+    username text not null, password text not null, age int, gender text, contact text, profile text, id serial primary key
 );
 create table chats(
     messages text not null
